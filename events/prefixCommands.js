0ec358f09f1b7f6268c2d3f7f3a683e9539d30e8
AroLeaf <starleafofa@gmail.com>
2022-10-13 10:33:43 +0200

    [fix] allow bots and webhooks to trigger Noelle again

---
+++ events/prefixCommands.js
@@ -0,0 +1,14 @@
+import { Event, Events } from '@aroleaf/djs-bot';
+import XRegExp from 'xregexp';
+
+export default new Event({
+  event: Events.MessageCreate,
+}, async message => {
+  const prefix = XRegExp(`^${XRegExp.escape(message.client.prefix)}`).exec(message.content)?.[0];
+  if (!prefix) return;
+
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const commandName = args.shift();
+  const command = message.client.commands.resolvePrefixCommand(commandName);
+  if (command) await command.execute(message, args);
+});
