38d13dc00abaf4fabe1d3a6f3d2de5c7df38c8bf
AroLeaf <starleafofa@gmail.com>
2022-10-22 10:30:12 +0200

    [feat] add leaderboard ER requirement

--- commands/leaderboard/leaderboard.js
+++ commands/leaderboard/leaderboard.js
@@ -1,12 +1,21 @@
-import { PrefixCommand } from '@aroleaf/djs-bot';
+import { PrefixCommand, PrefixCommandOptionType } from '@aroleaf/djs-bot';
 
 export default new PrefixCommand({
   name: 'leaderboard',
   description: 'View the Noelle Mains leaderboard.',
-}, async message => {
-  const top = message.client.leaderboard.toJSON().sort((a, b) => b.score - a.score).slice(0, 10);
+  options: [{
+    type: PrefixCommandOptionType.NUMBER,
+    name: 'er',
+    description: 'Energy recharge requirement, percentage',
+  }],
+}, async (message, args) => {
+  const ER = args.get('er');
+  const top = message.client.leaderboard.toJSON()
+    .filter(doc => !ER || doc.noelle.stats.parsed.ER >= (ER / 100))
+    .sort((a, b) => b.score - a.score)
+    .slice(0, 10);
   return message.reply({ embeds: [{
-    title: 'Noelle Mains Leaderboard',
+    title: `Noelle Mains Leaderboard ${ER ? `(≥${ER}% ER)` : ''}`,
     description: top.map((doc, i) => `#**${i + 1}**: **${Math.round(doc.score)}** by <@${doc.user}>`).join('\n'),
     color: 0xe17f93,
   }] });
--- events/prefixCommands.js
+++ events/prefixCommands.js
@@ -4,6 +4,7 @@ import XRegExp from 'xregexp';
 export default new Event({
   event: Events.MessageCreate,
 }, async message => {
+  if (message.guild?.id !== process.env.GUILD) return;
   const prefix = XRegExp(`^${XRegExp.escape(message.client.prefix)}`).exec(message.content)?.[0];
   if (!prefix) return;
 
