05e537bcb93a113e8ec15ce8d7a9aa4d35f47758
AroLeaf <starleafofa@gmail.com>
2022-10-22 12:10:29 +0200

    [feat] crit and nocrit leaderboards

--- commands/leaderboard/leaderboard.js
+++ commands/leaderboard/leaderboard.js
@@ -7,16 +7,35 @@ export default new PrefixCommand({
     type: PrefixCommandOptionType.NUMBER,
     name: 'er',
     description: 'Energy recharge requirement, percentage',
+    strict: false,
+  }, {
+    type: PrefixCommandOptionType.STRING,
+    name: 'sort',
+    description: 'What to sort by.',
   }],
 }, async (message, args) => {
   const ER = args.get('er');
+  const sort = args.get('sort') || 'average';
+  if (!['crit', 'nocrit', 'average'].includes(sort)) return message.reply('That is not a valid thing to sort by.');
+  
   const top = message.client.leaderboard.toJSON()
     .filter(doc => !ER || doc.noelle.stats.parsed.ER >= (ER / 100))
-    .sort((a, b) => b.score - a.score)
+    .sort((a, b) => {
+      switch (sort) {
+        case 'average': return b.score - a.score;
+        case 'crit': return b.noelle.damage.crit - a.noelle.damage.crit;
+        case 'nocrit': return b.noelle.damage.noCrit - a.noelle.damage.noCrit;
+      }
+    })
     .slice(0, 10);
+  
   return message.reply({ embeds: [{
     title: `Noelle Mains Leaderboard ${ER ? `(≥${ER}% ER)` : ''}`,
-    description: top.map((doc, i) => `#**${i + 1}**: **${Math.round(doc.score)}** by <@${doc.user}>`).join('\n'),
+    description: top.map((doc, i) => `#**${i + 1}**: **${Math.round({
+      average: doc.score,
+      crit: doc.noelle.damage.crit,
+      nocrit: doc.noelle.damage.noCrit,
+    }[sort])}** by <@${doc.user}>`).join('\n'),
     color: 0xe17f93,
   }] });
 });
