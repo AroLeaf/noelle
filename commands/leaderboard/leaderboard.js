b6d853e5229a6af3581b2c1696057c5785dc8bd0
AroLeaf <starleafofa@gmail.com>
2022-11-14 11:44:43 +0100

    [feat] add strict flag for filtering exact refinement and constellations

--- commands/leaderboard/leaderboard.js
+++ commands/leaderboard/leaderboard.js
@@ -69,6 +69,9 @@ export default new PrefixCommand({
       name: 'page',
       required: true,
     }],
+  }, {
+    name: 'strict',
+    short: 's',
   }],
   args: [{
     type: PrefixCommandOptionType.STRING,
