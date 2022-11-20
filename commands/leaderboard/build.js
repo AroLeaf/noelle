9be9e0d01648ea4415cf7b7c9ddacff25bb187c2
AroLeaf <starleafofa@gmail.com>
2022-11-20 20:32:37 +0100

    [feat] add --cached option to ~build to show leaderboard builds

--- commands/leaderboard/build.js
+++ commands/leaderboard/build.js
@@ -5,12 +5,16 @@ import { constants, EnkaError, Noelle, weapons, artifacts } from '../../lib/lead
 export default new PrefixCommand({
   name: 'build',
   description: `
-    Shows the build of a user. This will be their up-to-date build, and might not match the build they submitted to the leaderboard.
+    Shows the build of a user. This will be their current build, and might not match the build they submitted to the leaderboard.
     
     # \`[user]\`
     Can be either a discord user (mention or ID), or a Genshin UID. If a discord user is given, it tries to get their UID from the leaderboard's cache.
-    If no user is provided, it defaults to the user who issued the command.
+    If no user is given, it defaults to the user who issued the command.
   `,
+  options: [{
+    name: 'cached',
+    description: 'Show the build the user is on the leaderboard with instead of fetching their current build.',
+  }],
   args: [{
     type: PrefixCommandOptionType.USER,
     name: 'user',
@@ -18,15 +22,15 @@ export default new PrefixCommand({
     type: PrefixCommandOptionType.INTEGER,
     name: 'uid',
   }],
-}, async (message, { args }) => {
+}, async (message, { args, options }) => {
   const reply = content => message.reply({ content, allowedMentions: { parse: [], repliedUser: false } });
 
   const user = args.user || message.author;
   const uid = args.uid?.toString() || message.client.leaderboard.cache.get(user.id)?.uid;
 
-  if (!uid) return reply(`A cached UID could not be found for ${args.user ? 'the provided user' : 'you'}.`);
+  if (!uid) return reply(`A cached UID could not be found for ${args.user ? 'the given user' : 'you'}.`);
 
-  const noelle = await message.client.leaderboard.getBuild(uid).catch(async err => {
+  const noelle = options.cached ? (args.uid ? message.client.leaderboard.cache.find(entry => entry.uid === uid) : message.client.leaderboard.cache.get(user.id))?.noelle : await message.client.leaderboard.getBuild(uid).catch(async err => {
     if (!(err instanceof EnkaError)) throw err;
     return reply({
       [constants.ENKA_ERRORS.INVALID_UID]:          'The provided UID is invalid, or there is no user with that UID.',
@@ -34,6 +38,7 @@ export default new PrefixCommand({
       [constants.ENKA_ERRORS.CHARACTER_NOT_PUBLIC]: 'You don\'t have Noelle in your public characters.',
     }[err.code]);
   });
+  if (!noelle) return reply('A leaderboard entry could not be found for the given user.');
   if (!(noelle instanceof Noelle)) return;
 
   const artifactStats = {}
@@ -63,7 +68,7 @@ export default new PrefixCommand({
     - Talent LVLs: **${noelle.talentLevels.normal}**/**${noelle.talentLevels.skill}**/**${noelle.talentLevels.burst}**
 
     # Stats During Burst
-    - HP: **${burstStats.HP.toFixed(0)}**
+    - HP: **${burstStats.HP?.toFixed(0) || 'Not Found'}**
     - ATK: **${burstStats.ATK.toFixed(0)}**
     - DEF: **${burstStats.DEF.toFixed(0)}**
     - Crit Rate: **${(burstStats.CR * 100).toFixed(1)}%**
@@ -84,7 +89,7 @@ export default new PrefixCommand({
     #-Artifacts
     - CV: **${((artifactStats.CR * 2 + artifactStats.CD) * 100).toFixed(1)}**
     - ${artifactSets.length > 1 ? 'Sets' : 'Set'}: ${artifactSets.map(([k, v]) => `**${artifacts[k].name} (${v})**`).join(', ')}
-    - Main Stats: ${noelle.artifacts.filter(a => !['HP', 'ATK'].includes(a.mainStat)).map(a => `**${constants.PROPER_PROP_NAMES[a.mainStat]}${a.mainStat.startsWith('P') ? '%' : ''}**`).join('/')}
+    - Main Stats: ${noelle.artifacts.filter(a => a.mainStat && !['HP', 'ATK'].includes(a.mainStat)).map(a => `**${constants.PROPER_PROP_NAMES[a.mainStat]}${a.mainStat.startsWith('P') ? '%' : ''}**`).join('/') || '**Not found**'}
 
     # Damage
     - No crit: **${Math.round(damage.noCrit)}**
