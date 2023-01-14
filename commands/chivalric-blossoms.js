b6164bef532efdb4bd0d9411793cd96c3d42cd79
AroLeaf <starleafofa@gmail.com>
2023-01-14 15:57:15 +0100

    [fix] some spacing and grammar mistakes

--- commands/chivalric-blossoms.js
+++ commands/chivalric-blossoms.js
@@ -12,13 +12,15 @@ export default new PrefixCommand({
     # \`<cookies>\`
     *Quotation marks are not required for this argument*
     Your Hoyolab cookies. These can be obtained as follows:
+    
     - go to [your battle chronicle](https://act.hoyolab.com/app/community-game-records-sea/index.html#/ys)
     - open the browser developer console, this can usually be done with \`ctrl+shift+j\` / \`cmd+shift+j\` on Chrome, or with \`ctr+shift+i\` / \`cmd+shift+i\` and going to the "console" tab on most browsers.
     - paste \`copy(document.cookies)\` and press enter
+    
     Your cookies will now be on your clipboard.
 
     **WARNING!** Only give this bot your cookies if you trust me, its developer, Leaf#1950, admin of Noelle Mains!
-    **I promise I don't store these cookies, or using them for anything else than running the required checks for this command.**
+    **I promise I don't store these cookies, or use them for anything else than running the required checks for this command.**
     Having these cookies does **not** allow me to block you out of your account, and does **not** give me access to your Genshin account, only to your Hoyolab account.
   `,
 }, async (message, args) => {
