e41d0ba3506313e5f165b9c7a4cf0b4baae0e7b0
AroLeaf <starleafofa@gmail.com>
2023-01-16 21:12:36 +0100

    [feat] make event channels attachment-only

---
+++ events/messageCreate.js
@@ -0,0 +1,13 @@
+import { Event, Events } from '@aroleaf/djs-bot';
+
+export default new Event({
+  event: Events.MessageDelete,
+}, async message => {
+  if (![
+    '1064633680712175716',
+    '1064634114113810602',
+    '1064634458084487219',
+  ].includes(message.channel.id)) return;
+
+  if (message.content || message.stickers.size || message.embeds.length) return message.delete();
+});
