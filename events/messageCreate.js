821be47f783052f44a2bf3282498ef8fd3e6dbd3
AroLeaf <starleafofa@gmail.com>
2023-01-16 21:15:59 +0100

    [fix] give mods freedom

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,7 +3,7 @@ import { Event, Events } from '@aroleaf/djs-bot';
 export default new Event({
   event: Events.MessageCreate,
 }, async message => {
-  if (![
+  if (message.member?.roles.resolve('787242744170938379') ||![
     '1064633680712175716',
     '1064634114113810602',
     '1064634458084487219',
