a7c62d5c92573c0aeafa53ba400d45557b67ff98
AroLeaf <starleafofa@gmail.com>
2023-02-02 08:46:39 +0100

    [fix] update event channels

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,9 +4,8 @@ export default new Event({
   event: Events.MessageCreate,
 }, async message => {
   if (message.member?.roles.resolve('787242744170938379') ||![
-    '1064633680712175716',
-    '1064634114113810602',
-    '1064634458084487219',
+    '1070367414941913098',
+    '1070374685897928744',
   ].includes(message.channel.id)) return;
 
   if (message.content || message.stickers.size || message.embeds.length) return message.delete();
