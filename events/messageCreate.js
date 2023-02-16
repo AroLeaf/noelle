a67014c4da902b8444f4bf1b9cbc23b16687e73b
AroLeaf <starleafofa@gmail.com>
2023-02-16 19:30:47 +0100

    [feat] change event channels

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,8 +4,8 @@ export default new Event({
   event: Events.MessageCreate,
 }, async message => {
   if (message.member?.roles.resolve('787242744170938379') ||![
-    '1070367414941913098',
-    '1070374685897928744',
+    '1075829565395583096',
+    '1075829515034566716',
   ].includes(message.channel.id)) return;
 
   if (message.content || message.stickers.size || message.embeds.length) return message.delete();
