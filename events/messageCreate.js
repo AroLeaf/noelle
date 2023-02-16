71c73795a6077266f985e9d3c4b495117dbe53ff
AroLeaf <starleafofa@gmail.com>
2023-02-16 19:31:45 +0100

    [feat] put event channels in env instead

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,12 +1,11 @@
 import { Event, Events } from '@aroleaf/djs-bot';
 
+const eventChannels = process.env.EVENT_CHANNELS?.split(',');
+
 export default new Event({
   event: Events.MessageCreate,
 }, async message => {
-  if (message.member?.roles.resolve('787242744170938379') ||![
-    '1075829565395583096',
-    '1075829515034566716',
-  ].includes(message.channel.id)) return;
+  if (message.member?.roles.resolve('787242744170938379') ||!eventChannels.includes(message.channel.id)) return;
 
   if (message.content || message.stickers.size || message.embeds.length) return message.delete();
 });
